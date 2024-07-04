8fac3e87fc2d47eff1c2cb68df4b323424316d5b
prathmesh <prathmeshkarande511@gmail.com>
2024-07-04 22:35:30 +0530

    symbol example is added

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -17,20 +17,44 @@ let obj ={                                       // type (object)
     isLoggedIn:true
 };
 
-console.log(typeof(name), typeof(age), typeof(isLoggedIn), typeof(state), typeof(temp), typeof arr1, typeof arr2, typeof obj)
-console.table([typeof name, typeof age, typeof isLoggedIn, typeof state , typeof temp, typeof arr1, typeof arr2, typeof obj])
+const myFuntion = function(){
+    console.log("hello this is a function")
+}
+
+console.log(typeof(name), typeof(age), typeof(isLoggedIn), typeof(state), typeof(temp), typeof arr1, typeof arr2, typeof obj, typeof myFuntion)
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof state , typeof temp, typeof arr1, typeof arr2, typeof obj, typeof myFuntion])
+
+
+
+const val = 10 
+const val1 = 10.30
+const nm = "prathamesh"
+const bignum = 12625336774438n
+const isLogged = true
+const outsideTemp = null
+const someval = undefined;
+let somenum;
+
+const id = Symbol('123')        // unique id
+const anotherId = Symbol('123') // another unique id
+
+// let's  check are the are identical or different
+console.log (id === anotherId)  // false  (becasue they are unique)
+
+
 
 
 // Datatypes : 
-    // 1. primitive datatypes :
-    // number --> integer (range 2 pow 53) 
-    // bigint -->  range more than number
-    // String --> " "
-    // boolean --> true / false
-    // null -->  it type as well as stand alone value
-    // undefined --> not defined variable 
-    // symbol --> unique
+  // A. primitive datatypes : (total : 07)
+    // 1. number --> integer (range 2 pow 53) 
+    // 2. bigint -->  range more than number
+    // 3. String --> " "
+    // 4. boolean --> true / false
+    // 5. null -->  it type as well as stand alone value
+    // 6. undefined --> not defined variable 
+    // 7. symbol --> unique
     
-    // 2. non-primitive datatypes :
+  // B. non-primitive datatypes : (reference type)
     // array --> collection of related data items (type object)
     // objects --> instance of class or real world entity 
+    // functions --> can also be a non primitive data types
