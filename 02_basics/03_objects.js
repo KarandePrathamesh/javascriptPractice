e58c96cfb91b9388c3a50760a92fe59e4b08b063
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-01-22 11:30:40 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+
+
+// ***** Objects in Javascript *****
+
+// Methods of Declaring Objects 
+//      1. Literal                          // for eg. const JsUser = {}
+//      2. Constructor  --> Singleton       // for eg. Object.create
+
+//  1. Object Literals 
+const JsUser = {
+    name: "Prathamesh",
+    age: "22",
+    location:"Pune",
+    email:"karande@gmail.com",
+    isLoggedIn: false,
+    lastLoggedInDays:["monday", "friday",]
+
+}
