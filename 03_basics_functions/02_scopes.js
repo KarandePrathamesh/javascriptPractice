2917410364073863bbbc626e2400b9d4a20c32cd
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-01-31 09:40:08 +0000

    scope and nested scope of a variables

--- 03_basics_functions/02_scopes.js
+++ 03_basics_functions/02_scopes.js
@@ -27,3 +27,46 @@ console.log(`Global Scope c : ${c}`);       // therfore the var keyword does not
 
 
 
+// ********** nested Scope of variables in function block ***********
+//  example 1)
+function one(){
+    const userName = "Prathamesh"
+    function two(){
+        const website = "youtube"
+        console.log(userName);        
+        console.log(website);        
+    }
+    // console.log(website);            // error --> cuz website variable is not accesible out side the two() function scope
+    two()
+}
+one()
+
+// example 2)
+if(true){
+    const userName = "Prathamesh"
+    if(userName === "Prathamesh"){
+        const website = " youtube"
+        console.log(userName + website);
+    }
+    // console.log(website);            // error -->cuz website variable is not accesible out side the scope  
+}
+// console.log(userName);               // error -->cuz userName variable is not accesible out side the scope 
+
+
+
+// +++++++++++++++++++ intresting +++++++++++++++++++++
+// example 1)
+console.log(addOne(5));             // normal function can be called before the function definition 
+function addOne(num){
+    return num + 1
+}
+console.log(addOne(5));
+
+
+
+// example 2)
+// console.log(addTwo(5));             // erroe -> cuz variable decalred function cannot be accessed brfore initialization
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5));
