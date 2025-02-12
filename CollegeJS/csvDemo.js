d82caecc7045b014e07a593d0773ad49638f5a8d
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-02-12 06:41:46 +0000

    csv file reading and writing

--- CollegeJS/csvDemo.js
+++ CollegeJS/csvDemo.js
@@ -1,10 +1,35 @@
+// npm install csv-parser
+// npm install fast-csv
+
 const csv = require('csv-parser')
 const fs = require('fs')
 fs.createReadStream('cricket.csv')
-fs.appendFile(csv())
-fs.on('data',(row)=>{
-    console.log("csv row :",row);    
+.pipe(csv())        // begins to parse data into stream
+.on('data',(row)=>{
+    // console.log("csv row :",row);  
+    console.log(`${row['player_name']}`);  
+
 })
-fs.end('end',()=>{
+.on('end',()=>{
     console.log("read complete")
 })
+
+
+// Write to CSV files
+const csvWriter = require('fast-csv')
+const data = [
+    {player_name:'Onkar',player_surname:'Salunkhe'},
+    {player_name:'Rohan',player_surname:'Sapkal'},
+    {player_name:'Sakshi',player_surname:'Pawar'},
+    {player_name:'Prathamesh',player_surname:'Karande'},
+    {player_name:'Prashasti',player_surname:'Dhanorkar'},
+    {player_name:'Pranita',player_surname:'Birajdar'},
+    {player_name:'Gayatri',player_surname:'Astikar'},
+    {player_name:'Namarata',player_surname:'Akolkar'} ]
+
+    csvWriter.writeToPath('newData.csv',data,{headers:true})
+    .on('finish',()=>{
+        console.log("Write Complete");
+        
+    })
+
