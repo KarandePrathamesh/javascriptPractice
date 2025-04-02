5fc6cb8bae526930cbdf1d92f4488bc182d10fea
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-04-02 19:00:23 +0000

    conditional flow statements

---
+++ 04_control_flow/01_conditional_flow.js
@@ -0,0 +1,46 @@
+/**
+ * Control Flow Statements
+ *  1. Conditional Statements
+ *          a. if statement
+ *          b. if else statement
+ *          c. nested else if statement
+ *          d. switch statement
+ * 
+ *  2. Iterative Statements
+ * 
+ * 
+ * 
+ */
+
+
+
+//  prorgam1
+/* 
+if(2=="2"){
+    console.log("executed");            // op- executed
+} 
+*/  
+
+
+
+// program2
+/*
+if(2==="2"){
+    console.log("executed");            
+}else{
+    console.log("not executed");        // op- not executed
+}
+*/
+
+
+// program3
+//  one line scope
+/*
+let balance =1000;
+if(balance>500) console.log("test\n");            // not reable though
+
+//  multi line scope
+if(balance>500) console.log("test"),            // not reable though
+console.log("test2"),
+console.log("test3");
+*/
